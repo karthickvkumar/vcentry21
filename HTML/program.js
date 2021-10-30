7407e229bd4b427a34842c32e59a94d759b30a83
kvelraj_concord <kvelraj@concord.net>
2021-10-30 11:25:06 +0530

    js

---
+++ HTML/program.js
@@ -0,0 +1,18 @@
+// Number -> Any number value from Positive, Negative, Fractional, Decimal
+
+// var variable_name = value;
+
+// variable_name -> Any valid text, it should contains a-z, A-Z, 0-9, _, $
+
+var user_age = 28;
+console.log(user_age);
+
+// String -> Any text value which is enclosed inside a single and double quotes 
+
+var user_name = "Karthick Kumar";
+console.log(user_name);
+
+//Boolean -> Either true or false
+
+var result = true;
+console.log(result);
