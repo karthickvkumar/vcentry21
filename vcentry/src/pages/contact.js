07e3d1df12caa60986bd2d3a2d4f68ea9b35bd1b
kvelraj_concord <kvelraj@concord.net>
2021-11-20 11:44:07 +0530

    login form

--- vcentry/src/pages/contact.js
+++ vcentry/src/pages/contact.js
@@ -6,6 +6,17 @@ class ContactPage extends Component{
     return(
       <div>
         <h1>This is a Contact Page</h1>
+        <div className="m-bottom-8"> 
+          <label className="label">Enter your Email ID :</label>
+          <input className="input" type="text" placeholder="Please enter your email id.."/>
+        </div>
+        <div className="m-bottom-8">
+          <label className="label">Enter your Password :</label>
+          <input className="input" type="password" placeholder="Please enter your password.." />
+        </div>
+        <div>
+          <button>Login</button>
+        </div>
       </div>
     )
   }
