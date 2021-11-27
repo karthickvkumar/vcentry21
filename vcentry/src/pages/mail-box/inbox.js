caa9291776d17d2ea212616f7bd954bccc42a715
kvelraj_concord <kvelraj@concord.net>
2021-11-27 11:26:22 +0530

    get api

--- vcentry/src/pages/mail-box/inbox.js
+++ vcentry/src/pages/mail-box/inbox.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class InboxPage extends Component {
+
+  loadAPI(){
+    var url = "https://reqres.in/api/users?page=2";
+
+    axios.get(url)
+      .then((response) => {
+        var serverData = response.data;
+        console.log(serverData)
+      })
+      .catch((error) => {
+        console.log(error)
+      }) 
+  }
+
   render() {
     return (
       <div>
         <h1>This is a Inbox Page</h1>
+        <button onClick={() => this.loadAPI()}>Load User Data API</button>
       </div>
     );
   }
