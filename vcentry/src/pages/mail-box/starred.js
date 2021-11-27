86baeb39d55a991c59356f9eb7655cacb9d25556
kvelraj_concord <kvelraj@concord.net>
2021-11-27 12:47:28 +0530

    delete api

--- vcentry/src/pages/mail-box/starred.js
+++ vcentry/src/pages/mail-box/starred.js
@@ -31,6 +31,31 @@ class StarredPage extends Component {
       })
   }
 
+  onUpdate(){
+    const url = "https://reqres.in/api/users/2";
+
+    axios.put(url, this.state)
+      .then((response) => {
+        let serverData = response.data;
+        console.log(serverData)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  onDelete(){
+    const url = "https://reqres.in/api/users/2";
+
+    axios.delete(url)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   render() {
     return (
       <div>
@@ -44,7 +69,10 @@ class StarredPage extends Component {
           <input className="input" type="text" placeholder="Please enter your job .." onChange={this.onInputChange} name="job" />
         </div>
         <div>
-          <button onClick={() => this.onSubmit()}>Sumbit Profile</button>
+          <button onClick={() => this.onSubmit()}>Sumbit Profile (POST)</button>
+          <button onClick={() => this.onUpdate()}>Update Profile (PUT)</button>
+          <br></br>
+          <button onClick={() => this.onDelete()}>Delete Record</button>
         </div>
       </div>
     );
