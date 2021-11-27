b4f2bbae44be6d024a11fa0a20833785a7a24c92
kvelraj_concord <kvelraj@concord.net>
2021-11-27 11:59:21 +0530

    get api

--- vcentry/src/pages/mail-box/inbox.js
+++ vcentry/src/pages/mail-box/inbox.js
@@ -3,24 +3,58 @@ import axios from 'axios';
 
 class InboxPage extends Component {
 
-  loadAPI(){
-    var url = "https://reqres.in/api/users?page=2";
+  constructor(props) {
+    super(props);
+    this.state = {
+      userList: []
+    }
+  }
+
+  loadAPI() {
+    const url = "https://reqres.in/api/users?page=2";
 
     axios.get(url)
       .then((response) => {
-        var serverData = response.data;
-        console.log(serverData)
+        let serverData = response.data;
+        this.setState({
+          userList: serverData.data
+        })
       })
       .catch((error) => {
         console.log(error)
-      }) 
+      })
   }
 
   render() {
+    let users = this.state.userList.map((value, index) => {
+      return (
+        <tr key={index}>
+          <td>{value.id}</td>
+          <td>{value.first_name}</td>
+          <td>{value.last_name}</td>
+          <td>{value.email}</td>
+          <td>
+            <img src={value.avatar} className="dp-image" />
+          </td>
+        </tr>
+      )
+    });
+
     return (
       <div>
         <h1>This is a Inbox Page</h1>
         <button onClick={() => this.loadAPI()}>Load User Data API</button>
+        <table id="customers">
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Picture</th>
+          </tr>
+          {users}
+
+        </table>
       </div>
     );
   }
