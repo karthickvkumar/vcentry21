96308e6c1f198e0f6be327c42be575447d01bcb2
kvelraj_concord <kvelraj@concord.net>
2021-11-28 10:52:15 +0530

    lifecycle

--- vcentry/src/pages/mail-box/draft.js
+++ vcentry/src/pages/mail-box/draft.js
@@ -5,7 +5,7 @@ class DraftPage extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      message : "You are logged in successfully"
     };
     console.log("Triggered Constructor Method");
   }
@@ -18,12 +18,37 @@ class DraftPage extends Component {
     console.log("Triggered Component Did Mount Method");
   }
 
+  onChangeMessage(){
+    this.setState({
+      message : "Authentication Failed"
+    })
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log("Triggered Should Component Update Method");
+    console.log(nextState)
+    return true;
+  }
+
+  UNSAFE_componentWillUpdate(){
+    console.log("Triggered Component Will Update Method");
+  }
+  
+  componentDidUpdate(){
+    console.log("Triggered Component Did Update Method");
+  }
+
+  componentWillUnmount(){
+    console.log("Triggered Component Will Unmount Method");
+  }
 
   render() {
     console.log("Triggered Render Method");
     return (
       <div>
         <h1>This is a Draft Page</h1>
+        <h1>{this.state.message}</h1>
+        <button onClick={() => this.onChangeMessage()}>Update Message</button>
       </div>
     );
   }
